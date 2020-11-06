b5185875c1c75a990365e0958e3b62125eb49511
Smuliz <samuli.lindroos93@gmail.com>
2020-11-06 16:11:20 +0200

    commit säätöä... pipeline to fetch data from db to frontend working

--- Server/main/db.js
+++ Server/main/db.js
@@ -3,7 +3,7 @@ const {Pool} = require('pg');
 const pool = new Pool({
     user: 'samuli',
     host: 'localhost',
-    database: 'mediapalvelu',
+    database: 'mediapalvelut',
     password: '',
     port: 5432
 })
--- Server/main/routes.js
+++ Server/main/routes.js
@@ -8,24 +8,38 @@ router.get('/api/hello', (req, res) => {
 
 // GET all posts
 router.get('/api/get/allposts', (req,res,next) => {
+	try {
 	pool.query(`SELECT * FROM POSTS ORDER BY date_created DESC`,
 	(query_error, query_response) => {
 		res.json(query_response.rows)
 	})
+} catch (e) {
+	console.log("Error: " +  e.message);
+}
 });
 
 //GET one post by post_id
 router.get('/api/get/onepost', (req, res, next) => {
+	try {
 	const post_id = req.query.post_id;
+	console.log("POST_ID IN ROUTER GET ONE POST" + post_id);
 
 	pool.query(`SELECT * FROM posts WHERE post_id=$1`,
 	[ post_id ], (query_error, query_response) => {
-		res.json(query.response.rows)
+		console.log("Query_resp " + query_response);
+		if (query_response != undefined) {
+			res.json(query_response.rows);
+		}
+		
 	})
+} catch (e) {
+	console.log("Error: " + e.message);
+}
 });
 
 //POST a new post 
 router.post('/api/post/posttodb', (req ,res, next) => {
+	try {
 	const arvot = [
 		req.body.title,
 		req.body.post_body,
@@ -41,10 +55,14 @@ router.post('/api/post/posttodb', (req ,res, next) => {
 		} 
 		res.json(query_response.rows)
 	})
+} catch(e) {
+	console.log("Error: " + e.message);
+}
 });
 
 //UPDATE one post by post_id
 router.put('/api/put/updatepost', (req,res,next) => {
+	try {
 	const arvot = [
 		req.body.title,
 		req.body.post_body,
@@ -59,16 +77,23 @@ router.put('/api/put/updatepost', (req,res,next) => {
 		console.log(query_response);
 		console.log(query_error);
 	})
+} catch (e) {
+	console.log("Error: " + e.message);
+}
 });
 
 //DELETE one post by post_id
 router.delete('/api/delete/post', (req,res,next) => {
+	try {
 	const post_id = req.body.post_id;
 	pool.query(`DELETE FROM posts WHERE post_id = $1`, [post_id],
 	(query_error, query_response) => {
 		res.json(query_response.rows)
 		console.log("Query error while deleting: " + query_error);
 	})
+} catch (e) {
+	console.log("Error: " + e.message);
+}
 });
 
 
